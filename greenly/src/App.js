befb1e4deced32c4ecb3e21796c28dcfc17468d1
ethanvannest <ethan3vannest@gmail.com>
2022-11-14 12:48:44 -0700

    created link page files

--- greenly/src/App.js
+++ greenly/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/products/:id' component={ShowProduct}/>
+        <Route exact path='/cart' component={Cart}/>
+      </Switch>
     </div>
   );
 }
