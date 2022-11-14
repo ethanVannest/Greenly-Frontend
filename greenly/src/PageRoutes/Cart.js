d36c642f6f7f86faa77e5ae84dbec45e7c4b62e5
ethanvannest <ethan3vannest@gmail.com>
2022-11-14 12:55:02 -0700

    basic react page component setup

---
+++ greenly/src/PageRoutes/Cart.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Cart = () => {
+  return (
+    <div className='CartPage'>Cart</div>
+  )
+}
+
+export default Cart
---
+++ greenly/src/PageRoutes/ShowProduct.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ShowProduct = () => {
+  return (
+    <div className='ShowProductPage'>ShowProduct</div>
+  )
+}
+
+export default ShowProduct
