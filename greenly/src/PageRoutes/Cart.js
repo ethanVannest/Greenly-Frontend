10d4d0babf80ff245de973a8773be451dbc3c9de
ethanvannest <ethan3vannest@gmail.com>
2022-11-18 13:15:43 -0700

    created cart page + redux folders

--- greenly/src/PageRoutes/Cart.js
+++ greenly/src/PageRoutes/Cart.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import CartItems from '../components/CartItems'
+
 
 export const Cart = () => {
   return (
     <div className='CartPage'>
         <div>
             <h2>Cart</h2>
+
             <CartItems />
         </div>
         <div>
-
+            <div>
+                <p>Subtotal (0)</p>
+                <p>500</p>
+            </div>
+            <div>
+                <button type='button'>Proceed To Checkout</button>
+            </div>
         </div>
     </div>
   )
--- greenly/src/components/CartItems.js
+++ greenly/src/components/CartItems.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const CartItems = () => {
   return (
-    <div>CartItems</div>
+    <div>
+        <div>
+            <img src="https://media.direct.playstation.com/is/image/sierialto/PS5-front-with-dualsense" alt="Product"/>
+        </div>
+        <Link to={`/products/${1111}`}>
+            <p>Product 1</p>
+        </Link>
+
+        <p>500</p>
+        <select> 
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+        </select>
+        <button>
+            <i className='fas fa-trash'></i>
+        </button>
+    </div>
   )
 }
 
