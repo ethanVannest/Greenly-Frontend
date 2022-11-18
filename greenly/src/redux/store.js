570b1d1ffd8f1fcb3eab6c7f5ab927ded10f3df1
ethanvannest <ethan3vannest@gmail.com>
2022-11-18 13:20:44 -0700

    created store

--- greenly/src/redux/store.js
+++ greenly/src/redux/store.js
@@ -3,3 +3,16 @@ import { configureStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
 import {composeWithDevTools} from 'redux-devtools-extension'
+
+const reducer = combineReducers({
+
+})
+
+const middleware = {thunk}
+
+const store = configureStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
