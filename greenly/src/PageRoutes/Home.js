743c4412dba5ad5ec4b2fcbf8d827c81c5a522a4
ethanvannest <ethan3vannest@gmail.com>
2022-11-20 16:09:54 -0700

    connected front end and backend and actions to home page

--- greenly/src/PageRoutes/Home.js
+++ greenly/src/PageRoutes/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import Product from '../components/Products'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {indexOfProducts} from '../redux/actions/productActions'
 
 
-export const Home = () => {
+const Home = () => {
+
+    const dispatch = useDispatch()
+
+    const grabProduct = useSelector(state => state.grabProduct)
+
+    // const {products, loading, error} = grabProduct
+
+    useEffect(() => {
+        dispatch(indexOfProducts())
+    }, [dispatch])
+
     return (
         <div className='HomePage'>Home
             <h2>Featured Items</h2>
